a1185c91bf6a1fde4d85f35dc3ca8cfe4584aa51
lucastavaresuerj <lucastavaresuerj2016@gmail.com>
2019-10-10 16:05:13 -0300

    asasdasd

---
+++ paralelo6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <omp.h>
+#include "timer.h"
+
+int main(int argc, char **argv) {
+    const long N = 10000;
+    const long M = 100000;
+    const int T = 4;
+    double tempo, fim, inicio;
+    int i, j, cont;
+    GET_TIME(inicio);
+    #pragma omp parallel for num_threads(T) private(i,j,cont)
+    for (i=0; i<N; i++){
+        for (j=0; j<M; j++){
+            cont++;
+        }
+    }
+    GET_TIME(fim);
+    tempo = fim - inicio;
+    printf("Tempo: %.8lf\n", tempo);
+}
